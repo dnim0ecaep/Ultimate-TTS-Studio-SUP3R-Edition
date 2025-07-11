5e1cb3ecb5d73ca106814f68fd5b54d1fadcb282
Mrmateromatt <dnim0ecaep@gmail.com>
2025-07-11 15:26:51 -0400

    Create pre-load-requirements.sh

---
+++ pre-load-requirements.sh
@@ -0,0 +1,4 @@
+sudo apt install portaudio19-dev
+sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 -y
+wget -O anaconda.sh https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+bash anaconda.sh
